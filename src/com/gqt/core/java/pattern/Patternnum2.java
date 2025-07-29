d2061ea8e77f44f101bb602242c6c922cde4c62f
tnagaveni <tnagaveni1818@gmail.com>
2025-07-29 07:14:01 +0530

    Update Patternnum2.java

--- src/com/gqt/core/java/pattern/Patternnum2.java
+++ src/com/gqt/core/java/pattern/Patternnum2.java
@@ -20,7 +20,7 @@ public class Patternnum2 {
 		int n = sc.nextInt();
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
-				if(i+j==n/2|| j==n/2||i==n-1)
+				if(i==0|| j==n-1&&i<n/2||i==n-1||j==0&&i>n/2||i==n/2)
 				{
 					System.out.print("* ");
 				}
